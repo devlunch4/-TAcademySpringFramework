d8ccc511d96aa2fe3a326deef7bafe91ef5c2513
work@e470 <devlunch4@gmail.com>
2021-03-22 23:01:33 +0900

    [7강] DI 애플리케이션 작성(2)

--- src/myspring/di/xml/Hello.java
+++ src/myspring/di/xml/Hello.java
@@ -3,17 +3,18 @@ package myspring.di.xml;
 public class Hello {
 	private String name;
 	private Printer printer;
-	
-	public Hello() { }
-	
+
+	public Hello() {
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void setPrinter(Printer printer) {
 		this.printer = printer;
 	}
-	
+
 	public String sayHello() {
 		return "Hello " + name;
 	}
---
+++ src/myspring/di/xml/test/HelloBeanSpringTest.java
@@ -0,0 +1,36 @@
+package myspring.di.xml.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:config/beans.xml")
+public class HelloBeanSpringTest {
+	@Autowired
+	ApplicationContext context;
+
+	@Test
+	public void bean1() {
+		// 2. getBean() 호출
+		Hello hello = (Hello) context.getBean("hello");
+		// 3. Hello 의 sayHello() 호출.
+		assertEquals("Hello Spring", hello.sayHello());
+		;
+		// 3. Hello의 printer() 호출
+		hello.print();
+
+		// StringPrinter getBean()
+		Printer printer = context.getBean("printer", Printer.class);
+		assertEquals("Hello Spring", printer.toString());
+	}
+
+}
